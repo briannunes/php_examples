2aa691e20ca944c7902fac73bd65bc56c98c23d5
briannunes <32107502+briannunes@users.noreply.github.com>
2017-11-01 12:28:56 -0700

    Create hello.php

---
+++ hello.php
@@ -0,0 +1,21 @@
+<?php
+echo "
+<html>
+<META http-equiv=Content-Type content='text/html; charset=iso-8859-1'>
+
+<head>
+<META HTTP-EQUIV='Pragma' CONTENT='no-cache' NAME='Hello World - Example'>
+<META NAME='category' CONTENT='examples'>
+<META NAME='description' CONTENT='hello world'>
+<META NAME='keywords' CONTENT='hello world, hello world php example'>
+<meta NAME='author' CONTENT='Brian Nunes'>
+</head>
+
+<body>
+<H1>Hello World</H1>
+</body>
+
+</html>
+
+";
+?>
